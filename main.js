d60ffa2988ac94a9f5c9db7e40b79a405a8eec21
Martín Vázquez Torres <marvazto@gmail.com>
2017-06-30 14:17:08 +0200

    Update main.js

--- main.js
+++ main.js
@@ -134,7 +134,7 @@ client.on('message', message => {
     if (regexCallMe.test(message.content)) {
       message.channel.send('https://68.media.tumblr.com/f67ea264b93b8df0e558b61f019a2240/tumblr_o4rc1yh5ql1uulaizo1_500.gif')
     }
-    if (regexChancla.test(message.content)) {
+    if (regexBorja.test(message.content)) {
       message.channel.send('',{ file: 'https://cdn.discordapp.com/attachments/268398719802540032/330319777694220288/kek.jpg'})
     }
     
