07be61c71b7618def55734387e1eaff5c9ac612e
Martín Vázquez Torres <marvazto@gmail.com>
2017-07-04 15:16:51 +0200

    Update main.js

--- main.js
+++ main.js
@@ -172,8 +172,8 @@ client.on('guildMemberRemove', member => {
   }
 })
 
-  client.on('guildMemberSpeaking', member =>{
-    member.guild.defaultChannel.send('QUE TE CALLES, '+member.displayName)
-  })
+client.on('guildMemberSpeaking', member => {
+  member.guild.defaultChannel.send('QUE TE CALLES, '+ member.displayName)
+})
 
 client.login(token);
