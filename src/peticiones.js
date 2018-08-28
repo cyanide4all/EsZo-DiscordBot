efbad144cfa00d27e66813d0e5f067426993b240
Martín Vázquez Torres <marvazto@gmail.com>
2018-08-28 12:20:57 +0200

    Ahora si que fix

--- src/peticiones.js
+++ src/peticiones.js
@@ -1,6 +1,5 @@
 module.exports = (client) => {
   var regex = require("./regexp");
-  var galeguinhos = 0;
   client.on('message', message => { 
     if (regex.regexSiNo.test(message.content)) {
       let response = Math.random() < 0.5 ? -1 : 1;
@@ -12,10 +11,10 @@ module.exports = (client) => {
       message.reply(`PUES OBVIAMENTE, ${response}`).catch(console.log)
     }
     if (regex.regexCuantos.test(message.content)) {
-      if (message.guild.members.array().filter(each => regex.regexGale.test(each.user.username)).length > 0) {
-          galeguinhos = message.guild.members.array().filter(each => regex.regexGale.test(each.user.displayName)).length
-      }
-      let response = message.guild.memberCount - 1 + " DE LOS CUALES " + galeguinhos + " SON GALEGUINHO95 PORQUE ES PUTO TONTO. AH! Y YO, EL BOT, SOY EL BOT, YO ESTUVE, SÍ."
+      let response = 
+          message.guild.memberCount - 1 + " DE LOS CUALES " 
+          + message.guild.members.array().filter(each => regex.regexGale.test(each.displayName)).length 
+          + " SON GALEGUINHO95 PORQUE ES PUTO TONTO. AH! Y YO, EL BOT, SOY EL BOT, YO ESTUVE, SÍ."
       message.reply(response).then(() => galeguinhos = 0).catch(console.log)
     }
   })
