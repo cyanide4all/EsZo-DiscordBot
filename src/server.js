8a0a3a0208d75cfe25110954340d45519818f3da
Martín Vázquez Torres <marvazto@gmail.com>
2019-12-15 01:06:43 +0100

    Removed use of guild.defaultChannel

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ module.exports = (client) => {
     // Listener para cuando se viene alguien nuevo
     client.on('guildMemberAdd', member => {
         // El mensaje se manda al canal por defecto (boop)
-        member.guild.defaultChannel.send(`Bienvenido a nuestro servidor, ${member}.\nEs un lugar bonito y lleno de personas amables.\nQuédate si no maineas ni Bastion, ni Yasuo ni Symmetra.\nSi necesitas ayuda escribe '!halpmepls'`).catch(console.log);
+        member.guild.textChannels:toArray[0].send(`Bienvenido a nuestro servidor, ${member}.\nEs un lugar bonito y lleno de personas amables.\nQuédate si no maineas ni Bastion, ni Yasuo ni Symmetra.\nSi necesitas ayuda escribe '!halpmepls'`).catch(console.log);
     })
     
     // Listener para cuando alguien se pira o lo echamos (Aunque eso es poco probable)
@@ -10,12 +10,12 @@ module.exports = (client) => {
         // El mensaje se manda al canal por defecto (boop)
         member.guild.defaultChannel.send(`Nuestro querido miembro, ${member}, se ha ido para siempre.\nSe ruega una oración por su alma y la asistencia a la conducción`);
         if(member.roles.length > 0){
-            member.guild.defaultChannel.send('Sus roles eran:').catch(console.log)
+            member.guild.textChannels:toArray[0].send('Sus roles eran:').catch(console.log)
             // WIP--------------  TODO TODO
             for(var x in member.roles){
-                member.guild.defaultChannel.send(member.roles[x][role].name)
+                member.guild.textChannels:toArray[0].send(member.roles[x][role].name)
             }
-            member.guild.defaultChannel.send('Le echaremos de menos. O no.').catch(console.log)
+            member.guild.textChannels:toArray[0].send('Le echaremos de menos. O no.').catch(console.log)
             // WIP--------------  TODO TODO
         }
     })   
