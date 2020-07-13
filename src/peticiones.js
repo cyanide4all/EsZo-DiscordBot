78370f06a20eaeb6345369c629797d264ad70583
Cyanide4all <marvazto@gmail.com>
2020-07-13 16:54:56 +0200

    deleted cuantos somos

--- src/peticiones.js
+++ src/peticiones.js
@@ -10,12 +10,5 @@ module.exports = (client) => {
       }
       message.reply(`PUES OBVIAMENTE, ${response}`).catch(console.log)
     }
-    if (regex.regexCuantos.test(message.content)) {
-      let response = 
-          message.guild.memberCount - 1 + " DE LOS CUALES " 
-          + message.guild.members.array().filter(each => regex.regexGale.test(each.displayName)).length 
-          + " SON GALEGUINHO95 PORQUE ES PUTO TONTO. AH! Y YO, EL BOT, SOY EL BOT, YO ESTUVE, SÍ."
-      message.reply(response).then(() => galeguinhos = 0).catch(console.log)
-    }
   })
 }
