b649bed95bc37f1be25c1b7767c9dc4dad73fea7
Cyanide4all <marvazto@gmail.com>
2022-11-11 20:07:30 +0100

    Callate bot

--- index.js
+++ index.js
@@ -29,11 +29,4 @@ client.on("ready", () => {
   console.log("Doing some gud' ol' barrel rolls...");
 });
 
-client.on("messageCreate", (message) => {
-  console.log("success!");
-  if (message.author.id !== "317421133131677696") {
-    message.reply("test").catch(console.log);
-  }
-});
-
 client.login(process.env.discordToken || Config.discordToken);
